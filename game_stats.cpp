86fbb0b4c946c8401acfdac60006030d8e9405f1
Danyel Quintana Munoz <1057797@MY.SCCCD.EDU>
2025-10-06 14:08:30 -0700

    fixed 2nd drive sentencing

--- game_stats.cpp
+++ game_stats.cpp
@@ -9,7 +9,7 @@ int main() {
 
     int yards[SIZE] = { 67, 98, 60, 94, 35, 25, 45, 50, 30, 40 };
     cout << "First drive yards: " << yards[0] << endl;
-    cout << "Epic second drive: " << yards[1] << endl;
+    cout << "Epic second drive: " << yards[1] << " yards" << endl;
 
     return 0;
 }
